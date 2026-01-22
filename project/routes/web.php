9a7b1dfe5fb440b827b34f1178d513594912975e
KajE879 <9024897@student.zadkine.nl>
2026-01-22 12:38:28 +0100

    made web.php shorter

--- project/routes/web.php
+++ project/routes/web.php
@@ -3,19 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GebruikerController;
 
-Route::get('/register', function() { return view('gebruiker.register'); });
-Route::post('/register', [GebruikerController::class, 'register']);
-
-Route::get('/login', function() { return view('gebruiker.login'); });
+Route::resource('gebruikers', GebruikerController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
+Route::get('/login', [GebruikerController::class, 'loginForm']);
 Route::post('/login', [GebruikerController::class, 'login']);
-
-Route::get('/gebruikers/{id}/edit', function($id){
-    $gebruiker = App\Models\Gebruiker::find($id);
-    return view('gebruiker.edit', ['gebruiker' => $gebruiker]);
-});
-Route::put('/gebruikers/{id}', [GebruikerController::class, 'update']);
-Route::delete('/gebruikers/{id}', [GebruikerController::class, 'destroy']);
-
 Route::get('/dashboard', [GebruikerController::class, 'dashboard']);
 Route::get('/welcome', [GebruikerController::class, 'welcome']);
 
